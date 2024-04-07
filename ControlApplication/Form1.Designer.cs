4835e1af744ef3165817b971102c5ae45cf80764
Mantas Sabaliauskas <mantassabaliauskas22@gmail.com>
2024-04-07 19:37:32 +0300

    first commit

---
+++ ControlApplication/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace ControlApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connectButton = new System.Windows.Forms.Button();
+            this.staticStatusLabel = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.comPortDropdown = new System.Windows.Forms.ComboBox();
+            this.baudDropdown = new System.Windows.Forms.ComboBox();
+            this.serialOutput = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.stepButton = new System.Windows.Forms.Button();
+            this.voltageProbeDropdown = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.motorStartPositionInput = new System.Windows.Forms.NumericUpDown();
+            this.motorStepLengthInput = new System.Windows.Forms.NumericUpDown();
+            this.motorPositionStaticLabel = new System.Windows.Forms.Label();
+            this.motorPositionLabel = new System.Windows.Forms.Label();
+            this.stepStatusLabel = new System.Windows.Forms.Label();
+            this.stepStatusStaticLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.motorStartPositionInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.motorStepLengthInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(289, 12);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 0;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // staticStatusLabel
+            // 
+            this.staticStatusLabel.AutoSize = true;
+            this.staticStatusLabel.Location = new System.Drawing.Point(370, 17);
+            this.staticStatusLabel.Name = "staticStatusLabel";
+            this.staticStatusLabel.Size = new System.Drawing.Size(40, 13);
+            this.staticStatusLabel.TabIndex = 1;
+            this.staticStatusLabel.Text = "Status:";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(416, 17);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(10, 13);
+            this.statusLabel.TabIndex = 2;
+            this.statusLabel.Text = "-";
+            // 
+            // comPortDropdown
+            // 
+            this.comPortDropdown.FormattingEnabled = true;
+            this.comPortDropdown.Location = new System.Drawing.Point(12, 14);
+            this.comPortDropdown.Name = "comPortDropdown";
+            this.comPortDropdown.Size = new System.Drawing.Size(144, 21);
+            this.comPortDropdown.TabIndex = 3;
+            this.comPortDropdown.Text = "Select COM port";
+            // 
+            // baudDropdown
+            // 
+            this.baudDropdown.FormattingEnabled = true;
+            this.baudDropdown.Location = new System.Drawing.Point(162, 14);
+            this.baudDropdown.Name = "baudDropdown";
+            this.baudDropdown.Size = new System.Drawing.Size(121, 21);
+            this.baudDropdown.TabIndex = 4;
+            this.baudDropdown.Text = "Select baud";
+            // 
+            // serialOutput
+            // 
+            this.serialOutput.Location = new System.Drawing.Point(12, 219);
+            this.serialOutput.Name = "serialOutput";
+            this.serialOutput.ReadOnly = true;
+            this.serialOutput.Size = new System.Drawing.Size(776, 395);
+            this.serialOutput.TabIndex = 5;
+            this.serialOutput.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Motor step length, mm:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Voltage probe:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // stepButton
+            // 
+            this.stepButton.Location = new System.Drawing.Point(289, 72);
+            this.stepButton.Name = "stepButton";
+            this.stepButton.Size = new System.Drawing.Size(75, 23);
+            this.stepButton.TabIndex = 13;
+            this.stepButton.Text = "Step";
+            this.stepButton.UseVisualStyleBackColor = true;
+            this.stepButton.Click += new System.EventHandler(this.stepButton_Click);
+            // 
+            // voltageProbeDropdown
+            // 
+            this.voltageProbeDropdown.FormattingEnabled = true;
+            this.voltageProbeDropdown.Location = new System.Drawing.Point(162, 72);
+            this.voltageProbeDropdown.Name = "voltageProbeDropdown";
+            this.voltageProbeDropdown.Size = new System.Drawing.Size(121, 21);
+            this.voltageProbeDropdown.TabIndex = 14;
+            this.voltageProbeDropdown.Text = "Select voltage probe";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Motor starting position, mm:";
+            // 
+            // motorStartPositionInput
+            // 
+            this.motorStartPositionInput.DecimalPlaces = 3;
+            this.motorStartPositionInput.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.motorStartPositionInput.Location = new System.Drawing.Point(12, 111);
+            this.motorStartPositionInput.Name = "motorStartPositionInput";
+            this.motorStartPositionInput.Size = new System.Drawing.Size(144, 20);
+            this.motorStartPositionInput.TabIndex = 17;
+            this.motorStartPositionInput.ValueChanged += new System.EventHandler(this.motorStartPositionInput_ValueChanged);
+            // 
+            // motorStepLengthInput
+            // 
+            this.motorStepLengthInput.DecimalPlaces = 3;
+            this.motorStepLengthInput.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.motorStepLengthInput.Location = new System.Drawing.Point(12, 72);
+            this.motorStepLengthInput.Name = "motorStepLengthInput";
+            this.motorStepLengthInput.Size = new System.Drawing.Size(144, 20);
+            this.motorStepLengthInput.TabIndex = 18;
+            // 
+            // motorPositionStaticLabel
+            // 
+            this.motorPositionStaticLabel.AutoSize = true;
+            this.motorPositionStaticLabel.Location = new System.Drawing.Point(12, 136);
+            this.motorPositionStaticLabel.Name = "motorPositionStaticLabel";
+            this.motorPositionStaticLabel.Size = new System.Drawing.Size(114, 13);
+            this.motorPositionStaticLabel.TabIndex = 19;
+            this.motorPositionStaticLabel.Text = "Current Motor Position:";
+            // 
+            // motorPositionLabel
+            // 
+            this.motorPositionLabel.AutoSize = true;
+            this.motorPositionLabel.Location = new System.Drawing.Point(12, 149);
+            this.motorPositionLabel.Name = "motorPositionLabel";
+            this.motorPositionLabel.Size = new System.Drawing.Size(53, 13);
+            this.motorPositionLabel.TabIndex = 20;
+            this.motorPositionLabel.Text = "0.000 mm";
+            // 
+            // stepStatusLabel
+            // 
+            this.stepStatusLabel.AutoSize = true;
+            this.stepStatusLabel.Location = new System.Drawing.Point(416, 77);
+            this.stepStatusLabel.Name = "stepStatusLabel";
+            this.stepStatusLabel.Size = new System.Drawing.Size(10, 13);
+            this.stepStatusLabel.TabIndex = 22;
+            this.stepStatusLabel.Text = "-";
+            // 
+            // stepStatusStaticLabel
+            // 
+            this.stepStatusStaticLabel.AutoSize = true;
+            this.stepStatusStaticLabel.Location = new System.Drawing.Point(370, 77);
+            this.stepStatusStaticLabel.Name = "stepStatusStaticLabel";
+            this.stepStatusStaticLabel.Size = new System.Drawing.Size(40, 13);
+            this.stepStatusStaticLabel.TabIndex = 21;
+            this.stepStatusStaticLabel.Text = "Status:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 626);
+            this.Controls.Add(this.stepStatusLabel);
+            this.Controls.Add(this.stepStatusStaticLabel);
+            this.Controls.Add(this.motorPositionLabel);
+            this.Controls.Add(this.motorPositionStaticLabel);
+            this.Controls.Add(this.motorStepLengthInput);
+            this.Controls.Add(this.motorStartPositionInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.voltageProbeDropdown);
+            this.Controls.Add(this.stepButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.serialOutput);
+            this.Controls.Add(this.baudDropdown);
+            this.Controls.Add(this.comPortDropdown);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.staticStatusLabel);
+            this.Controls.Add(this.connectButton);
+            this.Name = "Form1";
+            this.Text = "Control Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.motorStartPositionInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.motorStepLengthInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Label staticStatusLabel;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.ComboBox comPortDropdown;
+        private System.Windows.Forms.ComboBox baudDropdown;
+        private System.Windows.Forms.RichTextBox serialOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button stepButton;
+        private System.Windows.Forms.ComboBox voltageProbeDropdown;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown motorStartPositionInput;
+        private System.Windows.Forms.NumericUpDown motorStepLengthInput;
+        private System.Windows.Forms.Label motorPositionStaticLabel;
+        private System.Windows.Forms.Label motorPositionLabel;
+        private System.Windows.Forms.Label stepStatusLabel;
+        private System.Windows.Forms.Label stepStatusStaticLabel;
+    }
+}
+
---
+++ ControlApplication/Form1.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlApplication
+{
+    enum ConnectionState
+    {
+        CONNECTING,
+        CONNECTED,
+        DISCONNECTING,
+        DISCONNECTED
+    }
+
+    enum ExecutionStatus
+    {
+        IDLE,
+        EXECUTING,
+        RESPONSE_RECEIVED
+    }
+
+    enum MotorStatus
+    {
+        HAS_NOT_MOVED,
+        HAS_MOVED
+    }
+
+    struct SerialCommandResponse
+    {
+        public string command;
+        public bool success;
+        public string data;
+    }
+    public partial class Form1 : Form
+    {
+        private ConnectionState connectionState = ConnectionState.DISCONNECTED;
+
+        static SerialPort _serialPort;
+
+        bool _continue = false;
+        ExecutionStatus executionStatus = ExecutionStatus.IDLE;
+        SerialCommandResponse globalCommandResponse;
+
+        private MotorStatus motorStatus = MotorStatus.HAS_NOT_MOVED;
+        int motorPosition = 0; //micrometers
+
+        string outputFilePath = @".\Output.csv";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (!File.Exists(outputFilePath))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(outputFilePath))
+                {
+                    sw.WriteLine("x,u");
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Read available COM ports and populate the dropdown
+            string[] ports = SerialPort.GetPortNames();
+            List<string> portList = new List<string>();
+            foreach (string port in ports)
+            {
+                portList.Add(port);
+            }
+
+            //If no ports are available, show it
+            if (portList.Count == 0)
+            {
+                comPortDropdown.Enabled = false;
+                connectButton.Enabled = false;
+                statusLabel.ForeColor = Color.Red;
+                statusLabel.Text = "No COM ports available";
+            }
+            else
+            {
+                comPortDropdown.DataSource = new BindingSource(portList, null);
+                comPortDropdown.SelectedIndex = 0;
+            }
+
+            //Populate the baud rate dropdown
+            List<int> baudList = new List<int> { 9600, 19200, 38400, 57600, 115200 };
+            baudDropdown.DataSource = new BindingSource(baudList, null);
+
+            //Populate the voltage proble dropdown
+            List<int> voltageProbeList = new List<int> {1, 2};
+            voltageProbeDropdown.DataSource = new BindingSource(voltageProbeList, null);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            if(connectionState == ConnectionState.DISCONNECTED)
+            {
+                //Display that connection is being established
+                connectButton.Enabled = false;
+                statusLabel.ForeColor = SystemColors.ControlText;
+                statusLabel.Text = "Connecting...";
+                connectionState = ConnectionState.CONNECTING;
+
+                //Establish connection
+                try
+                {
+                    _serialPort = new SerialPort();
+                    _serialPort.PortName = comPortDropdown.SelectedValue.ToString();
+                    _serialPort.BaudRate = Int32.Parse(baudDropdown.SelectedValue.ToString());
+                    _serialPort.Open();
+
+                    //Display that connection has been established
+                    connectButton.Enabled = true;
+                    statusLabel.ForeColor = Color.Green;
+                    statusLabel.Text = "Connected";
+                    connectionState = ConnectionState.CONNECTED;
+                    connectButton.Text = "Disconnect";
+
+                    //Set _continue to true, so that the serial port process can run
+                    _continue = true;
+                    ThreadPool.QueueUserWorkItem(o => mainSerial());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                    statusLabel.Text = "Error: " + ex.Message;
+                    connectionState = ConnectionState.DISCONNECTED;
+                    connectButton.Enabled = true;
+                    return;
+                }
+
+            } else if(connectionState == ConnectionState.CONNECTED)
+            {
+                //Set _continue to false, so that the serial port process can stop
+                _continue = false;
+
+                //Display that connection is being disconnected
+                connectButton.Enabled = false;
+                statusLabel.ForeColor = SystemColors.ControlText;
+                statusLabel.Text = "Disconnecting...";
+                connectionState = ConnectionState.DISCONNECTING;
+
+                //Disconnect
+                _serialPort.Close();
+
+                //Display that connection has been disconnected
+                connectButton.Enabled = true;
+                statusLabel.Text = "Disconnected";
+                connectionState = ConnectionState.DISCONNECTED;
+                connectButton.Text = "Connect";   
+            }
+        }
+
+        private void stepButton_Click(object sender, EventArgs e)
+        {
+            motorStatus = MotorStatus.HAS_MOVED;
+            motorStartPositionInput.Enabled = false;
+            motorStepLengthInput.Enabled = false;
+            stepButton.Enabled = false;
+
+            //Execute command on a different thread to avoid blocking the UI
+            string motorStepLength = motorStepLengthInput.Text;
+            string voltageProbe = voltageProbeDropdown.SelectedValue.ToString();
+            ThreadPool.QueueUserWorkItem(o => {
+                serialOutput.Invoke((MethodInvoker)delegate
+                {
+                    serialOutput.Text += "Moving motor by " + motorStepLength + " mm\n";
+                });
+
+                //Move motor
+                SerialCommandResponse motorResponse = executeCommandOverSerial("motor move " + motorStepLength);
+                //MessageBox.Show("Command: " + motorResponse.command + "\nSuccess: " + motorResponse.success + "\nData: " + motorResponse.data);
+
+                serialOutput.Invoke((MethodInvoker)delegate
+                {
+                    serialOutput.Text += "Reading voltage from probe " + voltageProbe + "\n";
+                });
+                //Measure voltage
+                SerialCommandResponse voltageResponse = executeCommandOverSerial("voltage get " + voltageProbe);
+                //MessageBox.Show("Command: " + voltageResponse.command + "\nSuccess: " + voltageResponse.success + "\nData: " + voltageResponse.data);
+
+                //Set step button to Enabled again
+                stepButton.Invoke((MethodInvoker)delegate
+                {
+                    stepButton.Enabled = true;
+                    stepButton.Focus();
+                });
+
+                //Update motor position
+                if (motorResponse.success)
+                {
+                    motorPosition = motorPosition + (int)(Double.Parse(motorStepLengthInput.Text) * 1000);
+                    motorPositionLabel.Invoke((MethodInvoker)delegate
+                    {
+                        motorPositionLabel.Text = ((double)motorPosition / 1000) + " mm";
+                    });
+
+                    using (StreamWriter sw = File.AppendText(outputFilePath))
+                    {
+                        sw.WriteLine(motorPosition + "," + voltageResponse.data);
+                    }
+                } else
+                {
+                    MessageBox.Show("Error: Motor did not move");
+                }
+            });
+        }
+
+        private void motorStartPositionInput_ValueChanged(object sender, EventArgs e)
+        {
+            if(motorStatus == MotorStatus.HAS_NOT_MOVED)
+            {
+                motorPosition = (int)(motorStartPositionInput.Value * 1000);
+                motorPositionLabel.Text = ((double)motorPosition/1000) + " mm";
+            }
+        }
+
+        private SerialCommandResponse executeCommandOverSerial(string command)
+        {
+            int numberOfDots = 0;
+            if(executionStatus != ExecutionStatus.IDLE)
+            {
+                stepStatusLabel.Invoke((MethodInvoker)delegate
+                {
+                    stepStatusLabel.Text = "Device is not idle. Waiting...";
+                });
+            }
+
+            while(executionStatus != ExecutionStatus.IDLE)
+            {
+                stepStatusLabel.Invoke((MethodInvoker)delegate
+                {
+                    string dots = new string('.', numberOfDots);
+                    stepStatusLabel.Text = "Device is not idle. Waiting" + dots;
+                    numberOfDots = (numberOfDots + 1) % 4;
+                });
+                Thread.Sleep(100);
+            }
+
+            executionStatus = ExecutionStatus.EXECUTING;
+            globalCommandResponse.command = command;
+            _serialPort.WriteLine(command);
+
+            while (executionStatus != ExecutionStatus.RESPONSE_RECEIVED)
+            {
+                stepStatusLabel.Invoke((MethodInvoker)delegate
+                {
+                    string dots = new string('.', numberOfDots);
+                    stepStatusLabel.Text = "Waiting for response" + dots;
+                    numberOfDots = (numberOfDots + 1) % 4;
+                });
+
+                Thread.Sleep(100);
+            }
+
+            executionStatus = ExecutionStatus.IDLE;
+
+            stepStatusLabel.Invoke((MethodInvoker)delegate
+            {
+                stepStatusLabel.Text = "-";
+            });
+
+            return globalCommandResponse;
+        }
+
+        private void mainSerial()
+        {
+            int responseState = 0;
+            while (_continue)
+            {
+                //System.Diagnostics.Debug.WriteLine("Reading line"); ;
+                try
+                {
+                    if (_serialPort.BytesToRead > 0)
+                    {
+                        string message = _serialPort.ReadLine();
+                        if (message != "")
+                        {
+                            System.Diagnostics.Debug.WriteLine(message);
+
+                            //Update the serial output textbox
+                            serialOutput.Invoke((MethodInvoker)delegate
+                            {
+                                serialOutput.Text += message + "\n";
+                            });
+
+                            if(executionStatus == ExecutionStatus.EXECUTING)
+                            {
+                                if(message.Contains("Success: "))
+                                {
+                                    responseState = 1;
+                                    string success = message.Substring(9, 1);
+                                    globalCommandResponse.success = success == "1";
+                                } else if(message.Contains("Data: "))
+                                {
+                                    if(responseState == 1)
+                                    {
+                                        globalCommandResponse.data = message.Replace("Data: ", "").Trim();
+                                        executionStatus = ExecutionStatus.RESPONSE_RECEIVED;
+                                        responseState = 0;
+                                    } else
+                                    {
+                                        MessageBox.Show("Error: Unexpected response, check output terminal for more details");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    Thread.Sleep(50);
+                } catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                    if(_serialPort.IsOpen)
+                    {
+                        _serialPort.Close();
+                    }
+                    connectionState = ConnectionState.DISCONNECTED;
+                    connectButton.Invoke((MethodInvoker)delegate
+                    {
+                        connectButton.Text = "Connect";
+                        connectButton.Enabled = true;
+                    });
+                    statusLabel.Invoke((MethodInvoker)delegate
+                    {
+                        statusLabel.Text = "Disconnected";
+                        statusLabel.ForeColor = Color.Red;
+                    });
+                    break;
+                }
+            }
+        }
+    }
+}
